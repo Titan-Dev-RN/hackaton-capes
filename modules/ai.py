19ca6671391a1659a87a57640abbffe23e6b8c8c
Gabriel <batistutag190@gmail.com>
2024-11-15 13:38:59 -0300

    fix bug read api key

--- modules/ai.py
+++ modules/ai.py
@@ -1,7 +1,8 @@
+from openai import api_key
 from pandasai import Agent
 import os
 import json
-with open('api_key.json', 'w') as secret_key:
+with open('api_key.json', 'r') as secret_key:
     API_KEY = json.load(secret_key)['api_key']
     os.environ["PANDASAI_API_KEY"] = API_KEY
 
